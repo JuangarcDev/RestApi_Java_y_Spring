2fdb851ee59d892c22da32183ce90767c7761970
JuangarcDev <juangarciagrr@gmail.com>
2023-07-21 23:05:59 -0500

    Se conecta con el repo local

---
+++ src/main/java/com/app/web/AppsaludSpringMysqlCrudApplication.java
@@ -0,0 +1,13 @@
+package com.app.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsaludSpringMysqlCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsaludSpringMysqlCrudApplication.class, args);
+	}
+
+}
